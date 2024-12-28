5161b5fad44ac471a283ce1b0c5190a7223e008e
gmecommerce <dunghackfacetao@gmail.com>
2024-12-28 23:21:17 +0700

    delete resource/view

--- resources/views/components/forms/form-errors.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['name'])
-
-@error($name)
-    <p class="text-xs text-error font-semibold mt-1">{{ $message }}</p>
-@enderror
--- resources/views/emails/contact.blade.php
+++
@@ -1,8 +0,0 @@
-<h1>New Contact Message</h1>
-<p>You have received a new message from the contact form on your website:</p>
-
-<p><strong>Name:</strong> {{ $details["name"] }}</p>
-<p><strong>Email:</strong> {{ $details["email"] }}</p>
-<p><strong>Message:</strong>{{ $details["message"] }}</p>
-
-<p>This email was sent from your website contact form.</p>
